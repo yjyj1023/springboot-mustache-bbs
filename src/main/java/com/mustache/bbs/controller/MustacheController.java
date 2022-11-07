afff2d21a7510a51f31d4e7599939b039c1663f9
yjyj1023 <lyj19961023@gmail.com>
2022-11-07 11:49:03 +0900

    PathVariable로부터 넘어온 값 넘기기

---
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -0,0 +1,23 @@
+package com.mustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model){
+        model.addAttribute("username","rok");
+        return "greetings";
+    }
+
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon2(@PathVariable String id, Model model){
+        model.addAttribute("username","rok");
+        model.addAttribute("id",id);
+        return "greetings";
+    }
+}
