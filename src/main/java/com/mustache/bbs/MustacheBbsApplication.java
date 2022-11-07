54e973264ad786639bc84f216a65d6ec4f3ac33b
yjyj1023 <lyj19961023@gmail.com>
2022-11-07 11:21:36 +0900

    first commit

---
+++ src/main/java/com/mustache/bbs/MustacheBbsApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheBbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustache/bbs/MustacheBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustacheBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
